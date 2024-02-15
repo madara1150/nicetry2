c287776d5339b4da9d73f7451257cf9b1e455264
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2024-02-15 19:07:26 +0700

    create : init project

---
+++ server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/madara/nicetry2/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "myuser"
+	password = "mypassword"
+	dbname   = "mydatabase"
+)
+
+func main() {
+
+	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	db.AutoMigrate(&models.User{})
+	fmt.Println("Database migration completed!")
+}
---
+++ server/models/user.go
@@ -0,0 +1,15 @@
+package models
+
+type User struct {
+	Id          string
+	Username    string
+	Password    string
+	Fname       string
+	Lname       string
+	Role        string
+	Email       string
+	PhoneNumber string
+	Info        string
+	Coin        float64
+	CreateAt    string
+}
