ac50af22f694b508f0d96374b16257747c2dff86
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2024-04-02 10:50:53 +0700

    nest : add mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
+import { MongooseModule } from '@nestjs/mongoose'
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, UsersController],
+  imports: [MongooseModule.forRoot('mongodb://root:example@localhost:27017/nicetry?authSource=admin'), UsersModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
